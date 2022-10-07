e1fcc5ccdb713d7e76149e036ac41484a65fdc4a
Andrii <andrii_babenko@external.mckinsey.com>
2022-10-07 21:00:02 +0200

    Test Project: Updated url redirect view to count amount of clicks on short url

--- link_transformer/views.py
+++ link_transformer/views.py
@@ -5,8 +5,8 @@ from django.http.response import HttpResponseRedirect, HttpResponse
 from django.views.defaults import bad_request, page_not_found
 from django.shortcuts import render
 from .forms import FullURLInputForm, URLHash
-from .models import URL, User, UserLink
-from .handlers import collect_user_data
+from .models import URL, User, UserLink, LinkClick
+from .handlers import collect_user_data, create_user_hash
 
 
 class FullURLInput(TemplateView):
@@ -35,8 +35,7 @@ class ShortURLOutput(View):
             return bad_request(request, None)
 
         # since there is no access control - collect user data to identify they
-        user_data = collect_user_data(request.META)
-        user_data_hash = hashlib.sha224(user_data.encode()).hexdigest()
+        user_data_hash = create_user_hash(request.META)
 
         # check user exists. If user doesn't - create one
         user = User.objects.filter(user_hash=user_data_hash).first()
@@ -49,42 +48,52 @@ class ShortURLOutput(View):
             url_id__origin_url=form.cleaned_data['input_url'], user_id__user_hash=user.user_hash
         ).first()
 
-        if user_link:
-            return HttpResponse("yes")
+        # create short link
+        if not user_link:
+            # generate short URL
+            url_hash = hashlib.shake_256(str(uuid.uuid4()).encode()).hexdigest(5)
+            short_url = f"{request.build_absolute_uri('/')}{url_hash}"
 
-        # generate short URL
-        url_hash = hashlib.shake_256(str(uuid.uuid4()).encode()).hexdigest(5)
-        short_url = f"{request.build_absolute_uri('/')}{url_hash}"
+            # get url instance. If URL doesn't exist create one
+            url = URL.objects.filter(origin_url=form.cleaned_data['input_url']).first()
+            if not url:
+                url = URL(origin_url=form.cleaned_data['input_url'])
+                url.save()
 
-        # get url instance. If URL doesn't exist create one
-        url = URL.objects.filter(origin_url=form.cleaned_data['input_url']).first()
-        if not url:
-            url = URL(origin_url=form.cleaned_data['input_url'])
-            url.save()
+            # create user link
+            user_link = UserLink(url_id=url, user_id=user, short_url=short_url, url_hash=url_hash)
+            user_link.save()
 
-        # create user link
-        user_link = UserLink(url_id=url, user_id=user, short_url=short_url)
-        user_link.save()
-
-        return render(request, 'link_transformer/short-url.html', {"short_url": short_url})
+        return render(request, 'link_transformer/short-url.html', {"short_url": user_link.short_url})
 
 
 class ShortURLDispatcher(RedirectView):
+    """
+    Short link redirect view
+    """
     def get(self, request, *args, **kwargs):
+        """
+        Process redirect to origin URL from short URL
+        :param request: incoming request with entered full url
+        :return: origin page based on short link, page not found if short link is not known
+        """
         form = URLHash(kwargs)
 
         # check if given data is valid
         if not form.is_valid():
             return page_not_found(request, None)
 
+        # generate user hash
+        user_data_hash = create_user_hash(request.META)
+
         # check if given url is present
-        db_data = URL.objects.filter(url_hash=form.cleaned_data['url_hash']).first()
-        if not db_data:
+        user_link = UserLink.objects.filter(
+            url_hash=form.cleaned_data['url_hash'], user_id__user_hash=user_data_hash
+        ).first()
+        if not user_link:
             return page_not_found(request, None)
 
-        # collect user data
-        user_data = collect_user_data(request.META)
-        user_data_hash = hashlib.sha224(user_data.encode()).hexdigest()
+        # save click on user short link
+        LinkClick(user_link=user_link).save()
 
-
-        return HttpResponseRedirect(db_data.origin_url)
+        return HttpResponseRedirect(user_link.url_id.origin_url)
