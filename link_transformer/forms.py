b312e057a05e9377ad9db4c5d134c943df62fce3
Andrii <andrii_babenko@external.mckinsey.com>
2022-10-07 09:26:56 +0200

    Test Project: added user data extraction

--- link_transformer/forms.py
+++ link_transformer/forms.py
@@ -3,6 +3,13 @@ from django import forms
 
 class FullURLInputForm(forms.Form):
     """
-    Full URL input form validator
+    Validate full URL input form validator
     """
     input_url = forms.URLField(label='input_url', max_length=500)
+
+
+class URLHash(forms.Form):
+    """
+    Validate URL hash
+    """
+    url_hash = forms.CharField(label='url_hash', max_length=10)
