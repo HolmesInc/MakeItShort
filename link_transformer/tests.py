c9b0d7b858f8576bb36c0726e537b595205f470d
Andrii <andrii_babenko@external.mckinsey.com>
2022-10-07 21:55:40 +0200

    Test Project: added base test for link_transformer index view

--- link_transformer/tests.py
+++ link_transformer/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class LinkTransformerIndexViewTests(TestCase):
+    def test_index(self):
+        """
+        Test best case for link_transformer index view
+        """
+        response = self.client.get(reverse('link_transformer:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name, ['link_transformer/index.html'])
